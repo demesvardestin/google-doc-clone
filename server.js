74efb3e2d340c126fad393f4403994d35a648460
demo07 <dddemesvar07@gmail.com>
2019-03-24 01:40:08 +0000

    fixed server.js error

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/gdoc-clone'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/gdoc-clone/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/gdoc-clone/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
