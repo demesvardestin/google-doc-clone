e67294357762779976b232a01ea9b66509dceb95
demo07 <dddemesvar07@gmail.com>
2019-03-24 01:46:14 +0000

    commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/gdoc-clone'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/gdoc-clone/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
